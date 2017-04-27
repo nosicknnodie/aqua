160f2eb04facf1e32482505326ee82323e57fc0f
nosicknnodie <nosicknnodie@naver.com>
2017-04-27 17:59:54 +0900

    몰라 바꼈어.

---
+++ src/main/java/net/ma/ri/user/vo/UserVo.java
@@ -0,0 +1,70 @@
+package net.ma.ri.user.vo;
+
+import java.util.Date;
+
+public class UserVo {
+    private int idx;
+    private String id;
+    private String email;
+    private String pass;
+    private int level;
+    private Date regDate;
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "UserVo{" +
+                "idx=" + idx +
+                ", id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                ", level=" + level +
+                ", regDate=" + regDate +
+                '}';
+    }
+}
